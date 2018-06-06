1880eb3fc36b50086dbe934aab34554b83ba3312
Szymon Dzialowski <sd@phase-ii.com>
2018-06-06 18:07:51 +0100

    cron.sh

---
+++ ssh-tunnel/cron.sh
@@ -0,0 +1,21 @@
+
+make isworking
+
+STATUS="$?";
+
+echo "is working status: $STATUS";
+
+if [ "$STATUS" != "0"]; then
+
+    echo 'attempt to start';
+
+    make start
+fi
+
+make isworking
+
+STATUS="$?";
+
+echo "is working status: $STATUS";
+
+echo 'end...';
